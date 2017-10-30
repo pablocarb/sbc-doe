184584862e986fbca7156675e255a2caf022aee3
mibsspc2 <pablo.carbonell@manchester.ac.uk>
2017-10-30 12:38:17 +0000

    ICE testing

--- doeopt.py
+++ doeopt.py
@@ -6,5 +6,8 @@ sys.path.append('/mnt/SBC1/code/ice-utils')
 ICE_INFO = os.path.join(os.getenv('HOME'), 'info/ice2.json')
 ICE_USER = 'Pablo Carbonell'
 ICE_EMAIL = 'pablo.carbonell@manchester.ac.uk'
+ICE_SERVER = "https://ice.synbiochem.co.uk"
+ICE_SERVER_TEST =  "https://testice.synbiochem.co.uk:8443/rest"
 ICE_RETRIEVE = 'ice' # server for queries
-ICE_POST = 'ice' # server for registration
+ICE_POST = 'icetest' # server for registration
+

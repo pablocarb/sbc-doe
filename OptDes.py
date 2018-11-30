723909be766596ee08ef75feb33db92b486b7b67
mibsspc2 <1038612+pablocarb@users.noreply.github.com>
2018-11-30 15:06:18 +0000

    Making a module

--- OptDes.py
+++ OptDes.py
@@ -46,7 +46,7 @@ def randExp( factors, n ):
 
 #%%
 def mapFactors( factors, M ):
-    # Map a experiment into [-1,1] range and create dummy variables for categorical factors
+    # Map a numerical factor into [-1,1] range, create dummy variables for categorical factors
     Mn = np.transpose( [np.ones( M.shape[0] )] )
     for i in np.arange( len(factors) ):
         v = factors[i]
@@ -89,6 +89,71 @@ def fullFactorial( factors ):
     return ffact
 
 #%%
+
+
+def DetMax( factors, n, m, w=1000, th=99.5, k=1 ):
+    # n: Number of runs
+    # m: sampled design space per iteration
+    # w: maximum iterations
+    # k: number of removal/additions per iteration
+    # th: stop criterium for D-efficiency (<= 100.0)
+    # Here I have implemented a simple DETMAX algorithm. At each iteration: 
+    #  - remove the design with the lowest variance
+    # - add the design with the highest variance
+    # Many more efficent variants exist (kl-exchange, genetic algorithms, etc..)
+    # See Mandal et al, Algorithmic searches for optimal design
+    
+    # Initial design: could we do something better than a purely random start?   
+    M = randExp( factors, n )
+    # Map factors into [-1,1] and convert chategorical
+    X = mapFactors( factors, M )
+
+    # D-Efficiency of the initial design
+    J = Deff(X)
+    print(J)
+    w = 0
+    while ((J<100.0) and (w < 1000)):
+        # X1 is the design space sample in the iteration. 
+        # Here we loop through the full factorial, which is computationally expensive
+        # First thing to fdo is to change it to random generation of a subset of the full library
+        # It would be better to move across some surface like gradient descent...
+        M1 = randExp( factors, m )
+        X1 = mapFactors( factors, M1 ) 
+        # Calcualte delta of removing an experiment
+        sub = []
+        for i in np.arange(X.shape[0]):
+            sub.append( VarAdd(X, X[i,:]) )
+        w += 1
+        # Remove the experiments with the lowest delta
+        Xsub = None
+        dList = np.argsort( sub )[0:(k-1)]
+        for i in np.arange(X.shape[0]):
+            if i in dList:
+                continue
+            else:
+                if Xsub is None:
+                    Xsub = X[i,:]
+                else:
+                    Xsub = np.vstack( [Xsub, X[i,:]] )
+        # Calculate the delta of adding an experiment from the sample
+        add = []
+        for j in np.arange(X1.shape[0]):
+            add.append( VarAdd( Xsub, X1[j,:] ) )
+        # Add the experiments with the highest delta
+        aList = np.flip( np.argsort( add ) )[0:(k+1)]
+        Xn = Xsub
+        for j in aList:
+            Xn = np.vstack( [Xn, X1[j,:] ] )
+        # Make the update id the resulting design increases the objective
+        #print(w,J,i,j, Dopt(X), Dopt(Xsub), Dopt(Xn))
+        if Dopt(Xn) > Dopt(X):
+            X = Xn
+            J = Deff(X)
+        elif Dopt(Xn) == Dopt(X):
+            break
+    print(w,J)
+    return X
+    
 
 n = 46 # Number of runs
 m = 100 # Sampled design space per iteration
